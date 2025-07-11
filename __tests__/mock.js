71947c184c8b8cb35b1bac60a79d4826c64390e8
Roman Podkorytov <romiraxp@mail.ru>
2025-07-11 16:35:02 +0500

    to check by a teacher

---
+++ __tests__/mock.js
@@ -0,0 +1,33 @@
+import { getLevel } from "../src/js/getlevel";
+import fetchData from "../src/js/http";
+jest.mock('../src/js/http');
+
+// перед каждым тестом сбрасываем все моки с помощью jest.resetAllMocks
+// все изначальные данные, которые в них могли бы быть
+beforeEach(() => { 
+    jest.resetAllMocks();
+});
+test('to get level about user if status is not OK', () => {
+    fetchData.mockReturnValue({ status: "", level: 10 });
+    getLevel(1);
+    expect(getLevel(1)).toBe("Информация об уровне временно недоступна"); 
+});
+
+beforeEach(() => { 
+    jest.resetAllMocks();
+});
+test('to get level about user if status is ok', () => {
+    fetchData.mockReturnValue({ status: "ok", level: 10 });
+    getLevel(1);
+    expect(getLevel(1)).toBe("Ваш текущий уровень: 10"); 
+});
+
+beforeEach(() => { 
+    jest.resetAllMocks();
+});
+test('A test on throw error message', () => {
+    //const url = "http://";
+    //expect(fetchData()).toThrow('Mock this!');
+    //expect(() => fetchData.mockReturnValue({ status: "ok", level: 10 })).toThrow(new Error('Mock this!'));
+    expect(fetchData).toThrow('Mock this!');
+});
---
+++ src/js/http.js
@@ -0,0 +1,6 @@
+// Демо-реализация функции fetchData (модуль http):
+export default function fetchData(url) {
+    if (!url) {
+        throw new Error('Mock this!');
+    }
+}
